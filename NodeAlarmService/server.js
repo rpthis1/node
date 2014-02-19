bb15eaba1938fbd86a9cc878bee89761dbcfddc1
rpthis1 <rpthis1@yahoo.com>
2014-02-19 17:19:28 +0000

    adding redis client

--- NodeAlarmService/server.js
+++ NodeAlarmService/server.js
@@ -1,11 +1,39 @@
 var http = require("http");
+var redis = require("redis");
 
+var client = redis.createClient("6379", "localhost");
+var client2 = redis.createClient("6379", "localhost");
 var options = {host: "http://localhost", path: "/rest/api/values" }
+var req;
 
-http.get("http://localhost/rest/api/values",function (res) {
-    console.log(res.responseText)
-}).on("error", function (error) {
+init();
+setInterval(getAlarms, 2000)
+
+
+function callBack (res) {
+
+    var bodyChunks = [];
+    res.on('data',function (chunk) {
+        bodyChunks.push(chunk);
+    }).on('end', function () {
+            var body = Buffer.concat(bodyChunks);
+            console.log('BODY: ' + body);
+
+            client.set("test", body.toString())
+        })
+};
+
+
+function init()
+{
+
+}
+function getAlarms() {
+    req = http.get("http://localhost/rest/api/alarm", callBack);
+    req.on("error", function (error) {
         console.log("Error" + error)
     });
+}
+
 
 
